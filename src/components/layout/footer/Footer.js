5843c83dfe80bb47d707e5ef4a649bbe45e98331
Paiva <pvnetto1@gmail.com>
2020-05-16 16:25:35 -0300

    add core footer styling

--- src/components/layout/footer/Footer.js
+++ src/components/layout/footer/Footer.js
@@ -8,34 +8,43 @@ import style from './Footer.module.scss';
 
 const Footer = () => {
 
-    // const data = useStaticQuery(graphql`
-    //     query {
-    //         autoforceLogoWhite: file (
-    //             relativePath: {eq: "logos/logo_autoforce_white.png"}
-    //         ) {
-    //             childImageSharp {
-    //                 fluid(maxWidth: 200) {
-    //                     ...GatsbyImageSharpFluid
-    //                 }
-    //             }
-    //         }
-    //     }
-    // `);
+    const data = useStaticQuery(graphql`
+        query {
+            groupLogo: file(
+                relativePath: { eq: "logos/logo_group.png" }
+            ) {
+                childImageSharp {
+                    fixed {
+                        ...GatsbyImageSharpFixed
+                    }
+                }
+            }
+
+            autoforceLogo: file (
+                relativePath: {eq: "logos/logo_autoforce_white.png"}
+            ) {
+                childImageSharp {
+                    fixed {
+                        ...GatsbyImageSharpFixed
+                    }
+                }
+            }
+        }
+    `);
 
     return (
         <footer className={style.footer}>
             <Container>
-
                 <Row>
                     <Col xs={3}>
-                        {/* <Img className={style.logo} fluid={data.autoforceLogoWhite.childImageSharp.fluid} /> */}
+                        <Link to="/"><Img fixed={data.groupLogo.childImageSharp.fixed} /></Link>
                     </Col>
-                    <Col xs={6} className="d-flex flex-column align-items-center justify-content-center text-center">
+                    <Col xs={6} className={`${style.copyrightSection} d-flex flex-column align-items-center justify-content-center text-center`}>
                         <p className="m-0"><small>Copyright © 2019 AutoForce - Todos os direitos reservados. </small></p>
                         <div className="d-flex">
-                            <Link>Visitar Site</Link>
-                            <Link>Contato</Link>
-                            <Link>Política de Privacidade</Link>
+                            <Link><small>Visitar Site</small></Link>
+                            <Link><small>Contato</small></Link>
+                            <Link><small>Política de Privacidade</small></Link>
                         </div>
                     </Col>
                     <Col xs={3}>
@@ -44,8 +53,10 @@ const Footer = () => {
                 </Row>
 
                 <Row>
-                    <Col className="mx-auto" xs={3}>
-                        {/* <Img className={style.logo} fluid={data.autoforceLogoWhite.childImageSharp.fluid} /> */}
+                    <Col className="mx-auto d-flex" xs={3}>
+                        <a className="m-auto" href="https://www.autoforce.com">
+                            <Img fixed={data.autoforceLogo.childImageSharp.fixed} />
+                        </a>
                     </Col>
                 </Row>
             </Container>
--- src/components/misc/socials/SocialIcons.js
+++ src/components/misc/socials/SocialIcons.js
@@ -1,9 +1,60 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram, faTwitter, faLinkedin, faYoutube, faFacebookF } from '@fortawesome/free-brands-svg-icons';
+
+import style from './SocialIcons.module.scss';
+import { useStaticQuery, graphql } from 'gatsby';
+
+const iconDict = {
+    instagram: {
+        icon: faInstagram,
+        hoverColor: '#DD336F'
+    },
+    twitter: {
+        icon: faTwitter,
+        hoverColor: '#1DA1F2'
+    },
+    facebook: {
+        icon: faFacebookF,
+        hoverColor: '#4367B2'
+    },
+    linkedin: {
+        icon: faLinkedin,
+        hoverColor: '#0277B5'
+    },
+    youtube: {
+        icon: faYoutube,
+        hoverColor: '#FF0402'
+    },
+}
 
 const SocialIcons = () => {
-    return (
-        <div>
+    const data = useStaticQuery(graphql`
+        query {
+            site {
+                siteMetadata {
+                    social {
+                        name
+                        url
+                    }
+                }
+            }
+        }
+    `);
 
+    const socials = data.site.siteMetadata.social;
+
+    return (
+        <div className={style.socialContainer}>
+            {socials.map((socialItem, idx) => {
+                const socialIcon = iconDict[socialItem.name];
+                if (socialIcon) return (
+                    <a key={idx} style={{ backgroundColor: socialIcon.hoverColor }} className={style.socialIcon} href={socialItem.url}>
+                        <FontAwesomeIcon className="my-auto" icon={socialIcon.icon} />
+                    </a>
+                );
+                else return null;
+            })}
         </div>
     );
 };
