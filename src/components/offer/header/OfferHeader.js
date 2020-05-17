6a437450c98d3983a95508aa5777a8ad4ad468ca
Paiva <pvnetto1@gmail.com>
2020-05-17 17:28:38 -0300

    add conditional styling to video bg

--- src/components/offer/header/OfferHeader.js
+++ src/components/offer/header/OfferHeader.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Img from 'gatsby-image';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import { useStaticQuery, graphql } from 'gatsby';
 
@@ -10,12 +11,12 @@ import style from './OfferHeader.module.scss';
 import FacebookButton from './facebook';
 import BackgroundVideo from '../../misc/BackgroundVideo';
 
-const OfferHeader = ({ name, price, versions, description, bannerImg, videoGallery }) => {
+const OfferHeader = ({ name, price, profileImg, versions, description, bannerImg, videoGallery }) => {
 
     const data = useStaticQuery(graphql`
         query {
-            headerBg: file(
-                relativePath: { eq: "header_bg.png" }
+            showcaseBg: file(
+                relativePath: { eq: "bg_showcase.png" }
             ) {
                 childImageSharp {
                     fluid(maxWidth: 1920) {
@@ -36,61 +37,75 @@ const OfferHeader = ({ name, price, versions, description, bannerImg, videoGalle
         }
     `);
 
+    const bgVideo = videoGallery[0];
+
     return (
         <Section className={`${style.section} position-relative`}>
             <header className={style.header} >
 
-                {videoGallery && videoGallery.length > 0 ?
+                {bgVideo ?
                     <BackgroundVideo videoURL={videoGallery[0]} /> :
-                    <BackgroundImage fluid={data.headerBg.childImageSharp.fluid} />}
+                    <BackgroundImage fluid={data.showcaseBg.childImageSharp.fluid} />}
 
                 <BackgroundImage fluid={data.colorBg.childImageSharp.fluid} className={style.overlay} />
+
                 <Container className="my-auto">
                     <Row>
                         <Col md={4} xs={12}>
                             <OfferConversionForm versions={versions} />
                         </Col>
                         <Col className="d-flex flex-column" md={8} xs={12}>
-                            <div>
-                                <div className={style.modelInfo}>
-                                    <h1 className={style.modelText}>
-                                        <strong>{name}</strong>
-                                    </h1>
+                            <Row className="h-100">
+                                <Col xs={6}>
+                                    <div className={style.modelInfo}>
+                                        <h1 className={style.modelText}>
+                                            <strong>{name}</strong>
+                                        </h1>
 
-                                    <h3 className={style.versionText}>
-                                        <strong>{versions && versions.length > 0 ? versions[0].name : ''}</strong>
-                                    </h3>
+                                        <h3 className={style.versionText}>
+                                            <strong>{versions && versions.length > 0 ? versions[0].name : ''}</strong>
+                                        </h3>
 
-                                    <h4 className="m-0">
-                                        <strong>{description}</strong>
-                                    </h4>
-                                </div>
-
-                                <div className={style.priceInfo}>
-                                    <h4 className="mb-0"><strong>De R$ <span>{price}</span></strong></h4>
-
-                                    <div className="d-flex flex-row align-items-end">
-                                        <h3><strong>Por R$ </strong></h3>
-                                        <h2><strong>{price}</strong></h2>
-                                    </div>
-                                </div>
-
-                                <p><small>*Consulte as Condições Comerciais</small></p>
-                            </div>
-
-                            <div className="d-flex flex-column align-items-center justify-content-center mt-auto">
-                                <div className="d-flex flex-column align-items-center justify-content-center">
-                                    <p className="mb-1"><small>Gostou? Então compartilhe.</small></p>
-                                    <div className="d-flex mb-4">
-                                        <FacebookButton className="mr-3" title="Share" value="378k" />
-                                        <FacebookButton title="Like" value="229k" />
+                                        <h4 className="m-0">
+                                            <strong>{description}</strong>
+                                        </h4>
                                     </div>
 
-                                    <p><small>*Imagens meramente ilustrativas. Alguns itens apresentados poderão não estar disponíveis nas versoes.
-                                        Preços sugeridos e válidos até 31/07/2015. Os preços poderão ser modificadas sem aviso prévio. Consulte e confirme
+                                    {price ? (
+                                        <div className={style.priceInfo}>
+                                            <h4 className="mb-0"><strong>De R$ <span>{price}</span></strong></h4>
+
+                                            <div className="d-flex flex-row align-items-end">
+                                                <h3><strong>Por R$ </strong></h3>
+                                                <h2><strong>{price}</strong></h2>
+                                            </div>
+                                            <p><small>*Consulte as Condições Comerciais</small></p>
+                                        </div>
+                                    ) : null}
+
+                                </Col>
+
+                                <Col className="d-flex" xs={6}>
+                                    <Img className={`${bgVideo ? 'd-md-none' : ''} d-block mt-auto w-100`} fluid={profileImg.childImageSharp.fluid} />
+                                </Col>
+                            </Row>
+
+                            <Row className="mt-auto">
+                                <Col xs={12} className="d-flex flex-column align-items-center justify-content-center mt-auto">
+                                    <div className="d-flex flex-column align-items-center justify-content-center">
+                                        <p className="mb-1"><small>Gostou? Então compartilhe.</small></p>
+                                        <div className="d-flex mb-4">
+                                            <FacebookButton className="mr-3" title="Share" value="378k" />
+                                            <FacebookButton title="Like" value="229k" />
+                                        </div>
+
+                                        <p className={style.disclaimer}><small>*Imagens meramente ilustrativas. Alguns itens apresentados poderão não estar disponíveis nas versoes.
+                                            Preços sugeridos e válidos até 31/07/2015. Os preços poderão ser modificadas sem aviso prévio. Consulte e confirme
                                         todas as informações com um de nossos vendedores..</small></p>
-                                </div>
-                            </div>
+                                    </div>
+                                </Col>
+
+                            </Row>
                         </Col>
                     </Row>
                 </Container>
